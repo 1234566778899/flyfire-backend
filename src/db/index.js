84098bd77822fb65b9b2c70c58f6755e524bd118
Carlos Jesús Ordaz Hoyos <carlos.ordaz@acomo.com.pe>
2024-09-29 18:44:11 -0500

    xx

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, {
+    //mongoose.connect('mongodb://localhost:27017/flyfar', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(db => console.log('db connected'))
